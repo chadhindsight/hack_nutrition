cd4689fb15e13f21a9a1c744785a49dd461be433
chad hinds <chadrickj8@gmail.com>
2024-01-21 11:17:40 -0500

    import food data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import foods from '../data/foods.json'
+
 import './App.css'
+console.log(foods)
 
 function App() {
-
+  // 
   return (
     <>
 
