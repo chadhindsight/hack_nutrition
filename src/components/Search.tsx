71a82299f68154dfd7eef0bfb412e0627d454b72
chad hinds <chadrickj8@gmail.com>
2024-01-24 16:56:18 -0500

    Continued setup for Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { Divider, Input } from 'antd';
+import { useAppContext } from '../context/globalState';
 
 function Search() {
+    const { dispatch } = useAppContext();
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const query = e.target.value;
+        console.log(query)
+        dispatch({ type: 'SEARCH_FOOD', payload: query });
+    };
+
     return (
         <>
-
+            <Divider>Search</Divider>
+            <label>Search Food</label>
+            <Input placeholder="Searh for food" type='text' onChange={(e) => handleSearch(e)} />
         </>
     );
 }
