513ba886b818496793b4bd381a496acb21f736f6
chad hinds <chadrickj8@gmail.com>
2024-02-07 13:09:34 -0500

    simplyfy wording

--- src/components/FoodBox.tsx
+++ src/components/FoodBox.tsx
@@ -21,7 +21,7 @@ function FoodBox(foodItem: foodData) {
                 <p>Calories: {calories}</p>
                 <p>Servings: {servings}</p>
                 <p>
-                    <b>Total Number of Calories: {calories * servings} </b> kcal
+                    <b>Total Calories: {calories * servings} </b> kcal
                 </p>
                 <Button type="primary" onClick={() => handleDelete(id)}> Delete </Button>
             </Card>
