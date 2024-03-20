2b1e4339a28bde9250cce9233fb1884f7352ad6e
chad hinds <chadrickj8@gmail.com>
2024-03-20 15:10:04 -0400

    Remove trailing comma

--- src/tests/FoodBox.test.tsx
+++ src/tests/FoodBox.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { describe, expect, it } from 'vitest';
 import FoodBox from '../components/FoodBox';
